242ba2263d52ec6ec459552c21493825ff9baf19
Marco Medrano <marco.medrano.garay@gmail.com>
2021-03-09 17:24:38 -0400

    added stun client to test a stun request

---
+++ stun-client/server.js
@@ -0,0 +1,13 @@
+const stun = require('stun');
+
+// const stunServer = 'stun.l.google.com:19302';
+const stunServer = 'localhost:3478';
+
+stun.request(stunServer, (err, res) => {
+    if (err) {
+        console.error(err);
+    } else {
+        const address = res.getXorAddress();
+        console.log('your ip', address);
+    }
+});
