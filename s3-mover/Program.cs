9dddc0fa96a20643c23f563252058dd0dae8fb77
Marco Medrano <marco.medrano@talkdesk.com>
2021-04-07 16:34:07 -0400

    added project to watch files

---
+++ s3-mover/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace s3_mover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("S3 Mover");
+            Console.WriteLine("Running on " +  Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+            var watcher = new FileSystemWatcher();
+            watcher.Path = args == null && args.Length > 0 ? args[0] : ".";
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                                   | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Filter = "*.webm";
+            watcher.Changed += OnChanged;
+            watcher.EnableRaisingEvents = true;
+
+            Console.ReadKey();
+        }
+
+        private static void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"{e.Name} {e.FullPath}");
+        }
+    }
+}
